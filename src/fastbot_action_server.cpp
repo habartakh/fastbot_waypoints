5f3e804e5e4d42cab3be39486c723e17d397aa26
habartakh <bartakh.hajar@gmail.com>
2025-08-13 15:39:54 +0000

    Corrected logic behind action server execution

--- src/fastbot_action_server.cpp
+++ src/fastbot_action_server.cpp
@@ -23,7 +23,7 @@ class WaypointActionServer : public rclcpp::Node {
 public:
   explicit WaypointActionServer(
       const rclcpp::NodeOptions &options = rclcpp::NodeOptions())
-      : Node("fastbot_as", options), yaw_precision_(M_PI / 90.0),
+      : Node("fastbot_as", options), yaw_precision_(0.05),
         dist_precision_(0.05), state_("idle") {
     using namespace std::placeholders;
     action_server_ = rclcpp_action::create_server<Waypoint>(
@@ -32,10 +32,11 @@ public:
         std::bind(&WaypointActionServer::handle_cancel, this, _1),
         std::bind(&WaypointActionServer::handle_accepted, this, _1));
 
-    cmd_vel_pub_ =
-        this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 1);
+    cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>(
+        "/fastbot/cmd_vel", 1);
     odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
-        "/odom", 10, std::bind(&WaypointActionServer::odom_callback, this, _1));
+        "/fastbot/odom", 10,
+        std::bind(&WaypointActionServer::odom_callback, this, _1));
 
     RCLCPP_INFO(this->get_logger(), "Fastbot Waypoint Action Server started");
   }
@@ -47,10 +48,16 @@ private:
 
   geometry_msgs::msg::Point position_;
   double yaw_{0.0};
+  double initial_yaw = 1.46; // The robot spawns with an offset
   std::string state_;
 
   double yaw_precision_, dist_precision_;
   geometry_msgs::msg::Point des_pos_;
+
+  bool success = false; // If the robot successfully reached the goal
+
+  enum class State { FIX_YAW, GO_TO_POINT, FIX_FINAL_YAW };
+  State state = State::FIX_YAW;
 
   rclcpp_action::GoalResponse
   handle_goal(const rclcpp_action::GoalUUID &uuid,
@@ -83,12 +90,17 @@ private:
     auto feedback = std::make_shared<Waypoint::Feedback>();
     auto result = std::make_shared<Waypoint::Result>();
 
-    while (rclcpp::ok()) {
+    while (rclcpp::ok() && !success) {
+
       double dx = des_pos_.x - position_.x;
       double dy = des_pos_.y - position_.y;
       double err_pos = std::hypot(dx, dy);
-      double desired_yaw = std::atan2(dy, dx);
-      double err_yaw = desired_yaw - yaw_;
+      double desired_yaw = normalize_angle(std::atan2(dy, dx));
+      double err_yaw = normalize_angle(desired_yaw - (yaw_ - initial_yaw));
+
+      // RCLCPP_INFO(this->get_logger(), "err_pos = %f", err_pos);
+      // RCLCPP_INFO(this->get_logger(), "desired_yaw = %f", desired_yaw);
+      // RCLCPP_INFO(this->get_logger(), "err_yaw = %f", err_yaw);
 
       if (goal_handle->is_canceling()) {
         result->success = false;
@@ -98,13 +110,49 @@ private:
       }
 
       geometry_msgs::msg::Twist twist;
-      if (std::fabs(err_yaw) > yaw_precision_) {
-        state_ = "fix yaw";
-        twist.angular.z = (err_yaw > 0) ? 0.65 : -0.65;
-      } else if (err_pos > dist_precision_) {
-        state_ = "go to point";
-        twist.linear.x = 0.6;
-      } else {
+
+      switch (state) {
+      case State::FIX_YAW:
+        if (std::fabs(err_yaw) > yaw_precision_) {
+          state_ = "fix yaw";
+          RCLCPP_INFO(this->get_logger(), "State: fix yaw");
+          twist.angular.z = (err_yaw > 0) ? 0.25 : -0.25;
+          twist.linear.x = 0.0;
+
+        } else {
+          twist.angular.z = 0.0;
+          state = State::GO_TO_POINT;
+          continue;
+        }
+        break;
+
+      case State::GO_TO_POINT:
+        if (err_pos < dist_precision_) {
+          state = State::FIX_FINAL_YAW;
+          continue;
+        }
+        if (std::fabs(err_yaw) > yaw_precision_) {
+          state = State::FIX_YAW;
+          continue;
+        }
+        twist.angular.z = 0.0;
+        twist.linear.x = 0.4;
+        RCLCPP_INFO(this->get_logger(), "State: Go to Point");
+        break;
+
+      case State::FIX_FINAL_YAW:
+        RCLCPP_INFO(this->get_logger(), "State: fix final yaw");
+        if (std::fabs(err_yaw) > yaw_precision_) {
+          state_ = "fix final yaw";
+          RCLCPP_INFO(this->get_logger(), "State: fix final yaw 2");
+          twist.angular.z = (err_yaw > 0) ? 0.15 : -0.15;
+          twist.linear.x = 0.0;
+        } else {
+          // Finished movement
+          twist.angular.z = 0.0;
+          twist.linear.x = 0.0;
+          success = true;
+        }
         break;
       }
 
@@ -118,10 +166,11 @@ private:
     }
 
     // Stop robot
-    geometry_msgs::msg::Twist twist;
-    cmd_vel_pub_->publish(twist);
+    geometry_msgs::msg::Twist stop_twist;
+    cmd_vel_pub_->publish(stop_twist);
 
-    result->success = true;
+    // Return success
+    result->success = success;
     goal_handle->succeed(result);
     RCLCPP_INFO(this->get_logger(), "Reached goal successfully");
   }
@@ -135,6 +184,18 @@ private:
     tf2::Matrix3x3 m(q);
     double roll, pitch;
     m.getRPY(roll, pitch, yaw_);
+  }
+
+  // normalize angles to range [-pi, pi]
+  double normalize_angle(double angle) {
+
+    while (angle > M_PI) {
+      angle -= 2 * M_PI;
+    }
+    while (angle < -M_PI) {
+      angle += 2 * M_PI;
+    }
+    return angle;
   }
 };
 
